8dadf69007443bed61cff47c48a5ddba1476cc5a
LikeDrinkingWater_glb <LikeDrinkingWater_glb@chl.com>
2019-01-13 23:35:46 +0800

    hello Spring, JUnit and log4j

--- src/main/Java/com/huanleichen/hello/spring/MyTest.java
+++ src/main/Java/com/huanleichen/hello/spring/MyTest.java
@@ -1,13 +1,21 @@
 package com.huanleichen.hello.spring;
 
 import com.huanleichen.hello.spring.service.Hi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
+    public static final Logger logger = LoggerFactory.getLogger(MyTest.class);
+
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-context.xml");
         Hi hi = (Hi)applicationContext.getBean("Hi");
         hi.sayHi();
+
+        logger.info("slf4j for java");
+        logger.warn("slf4j for java");
+        logger.error("slf4j for java");
     }
 }
